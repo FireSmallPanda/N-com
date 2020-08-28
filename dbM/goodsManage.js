6f496ef150d1c78437958fc480bd57e0021e75d4
weihao_ling <1020529941@qq.com>
2020-08-28 14:08:09 +0800

    fix:新增为搜索模糊查询

--- dbM/goodsManage.js
+++ dbM/goodsManage.js
@@ -1,14 +1,24 @@
 var mysql = require('mysql');
 let { connection } = require('../utils/dbMysql')
 // 查询商品
-exports.queryGoods = async () => {
+exports.queryGoods = async (modSqlParams) => {
+  let sql = 'SELECT * from goods where 1 = 1'
+  let params = [] 
+  for(key in modSqlParams){
+    if(key=='name'){
+      sql+=` and ${key} like ?`
+      params.push("%"+modSqlParams[key]+"%")
+    }else{
+      sql+=` and ${key} = ?`
+      params.push(modSqlParams[key])
+    }
+    
+  }
   let retn = await new Promise((resolve, reject) => {
-    connection.query('SELECT * from goods', function (error, results, fields) {
+    connection.query(sql,params, function (error, results, fields) {
       if (error) throw error;
       resolve(results)
     });
   });
-  // 显示查询结果
-  console.log(retn)
   return retn;
 }
--- service/goodsService.js
+++ service/goodsService.js
@@ -5,8 +5,7 @@ let goodsManage = require("../dbm/goodsManage");
 exports.getGoods = async (req, res) => {
   let content = {}
   content.success = true
-  content.data = await goodsManage.queryGoods();
-  console.log(JSON.stringify(content))
+  content.data = await goodsManage.queryGoods(req.query);
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(content));
 }
